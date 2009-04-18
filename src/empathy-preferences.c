a9d1b4447ea461210ee9f79ddeb709a13787d4f8
Lars-Peter Clausen <lars@metafoo.de>
2009-04-18 03:18:15 +0200

    Fix memleak. Free strings retrieved from gtk_tree_model_get.

--- src/empathy-preferences.c
+++ src/empathy-preferences.c
@@ -289,6 +289,7 @@ preferences_sound_cell_toggled_cb (GtkCellRendererToggle *toggle,
 
 	empathy_conf_set_bool (empathy_conf_get (), key, instore);
 
+	g_free (key);
 	gtk_tree_path_free (path);
 }
 
@@ -566,6 +567,7 @@ preferences_languages_load_foreach (GtkTreeModel  *model,
 		}
 	}
 
+	g_free (code);
 	gtk_list_store_set (GTK_LIST_STORE (model), iter, COL_LANG_ENABLED, found, -1);
 	return FALSE;
 }
