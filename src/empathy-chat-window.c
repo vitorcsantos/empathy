ab30e5cff24a01f75cb0f08b138b13504253e2c0
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2010-11-30 14:58:09 +0100

    chat-window: don't display notifications for pending messages (#635513)

--- src/empathy-chat-window.c
+++ src/empathy-chat-window.c
@@ -1426,7 +1426,11 @@ chat_window_new_message_cb (EmpathyChat       *chat,
 
 		empathy_sound_play (GTK_WIDGET (priv->dialog),
 		    EMPATHY_SOUND_MESSAGE_INCOMING);
-		chat_window_show_or_update_notification (window, message, chat);
+
+		/* Pending messages have already been displayed in the approver, so we don't
+		* display a notification for those. */
+		if (!pending)
+			chat_window_show_or_update_notification (window, message, chat);
 	}
 
 	/* update the number of unread messages and the window icon */
