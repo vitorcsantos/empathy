vitorcsantos/empathy

commit c5220a248d7626129eadce95a606eb4157aafe71

account_chooser_update_iter: don't leak the TpAccount

libempathy-gtk/empathy-account-chooser.c
@@ -826,6 +826,8 @@ account_chooser_update_iter (EmpathyAccountChooser *chooser,
 			      (gpointer) data, priv->filter_data);
 	else
 		account_chooser_filter_ready_cb (TRUE, (gpointer) data);
+
+	g_object_unref (account);
 }
 
 static void

commit c4bfd9ec3f2df6e38b66023d82bc6c17c8ed4348

Give the FilterResultData a reference to account
This partially reverts the regression introduce in
c5220a248d7626129eadce95a606eb4157aafe71, but gives a more explicit
reference to the data structure which will hopefully make the code more
clear

libempathy-gtk/empathy-account-chooser.c
@@ -818,7 +818,7 @@ account_chooser_update_iter (EmpathyAccountChooser *chooser,
 
 	data = g_slice_new0 (FilterResultCallbackData);
 	data->chooser = chooser;
-	data->account = account;
+	data->account = g_object_ref (account);
 	data->iter = g_memdup (iter, sizeof (GtkTreeIter));
 
 	if (priv->filter)