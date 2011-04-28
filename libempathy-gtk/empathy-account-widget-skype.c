vitorcsantos/empathy

commit ad4626eb7c092ecdd14db38b425d9ebf592278f3

Disconnect other psyke accounts when creating a new one

libempathy-gtk/empathy-account-widget-skype.c
@@ -962,6 +962,9 @@ static gboolean
 is_other_psyke_account (TpAccount *ours,
     TpAccount *other)
 {
+  if (ours == NULL)
+    return TRUE;
+
   return (tp_strdiff (
         tp_proxy_get_object_path (ours),
         tp_proxy_get_object_path (other)) &&

libempathy-gtk/empathy-account-widget.c
@@ -866,7 +866,9 @@ static void
 account_widget_apply_clicked_cb (GtkWidget *button,
     EmpathyAccountWidget *self)
 {
-    account_widget_apply_and_log_in (self);
+  empathy_accounts_dialog_skype_disable_other_accounts (NULL, NULL);
+
+  account_widget_apply_and_log_in (self);
 }
 
 static void

commit 0f3a239c71f4f9fab63ab32c637f4aaa4a811417

Fix logic error introduced in ad4626eb

libempathy-gtk/empathy-account-widget-skype.c
@@ -962,12 +962,16 @@ static gboolean
 is_other_psyke_account (TpAccount *ours,
     TpAccount *other)
 {
-  if (ours == NULL)
-    return TRUE;
+  gboolean paths_diff;
 
-  return (tp_strdiff (
+  if (ours == NULL)
+    paths_diff = TRUE;
+  else
+    paths_diff = tp_strdiff (
         tp_proxy_get_object_path (ours),
-        tp_proxy_get_object_path (other)) &&
+        tp_proxy_get_object_path (other));
+
+  return (paths_diff &&
       tp_account_is_enabled (other) &&
       !tp_strdiff (tp_account_get_connection_manager (other), "psyke"));
 }