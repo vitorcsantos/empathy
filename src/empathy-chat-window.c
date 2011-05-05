de1d1ad186ec7bcabd83aa653f8c766d8fa05fbe
Danielle Madeley <danielle.madeley@collabora.co.uk>
2011-05-05 16:13:19 +1000

    Revert "Make empathy_chat_window_find_chat() static"
    This reverts commit e84d41efe4b2febb9f3276e54f603550c929a69d.

--- src/empathy-chat-window.c
+++ src/empathy-chat-window.c
@@ -146,8 +146,6 @@ static const GtkTargetEntry drag_types_dest_file[] = {
 
 static void chat_window_update (EmpathyChatWindow *window,
 		gboolean update_contact_menu);
-static EmpathyChat *empathy_chat_window_find_chat (TpAccount *account,
-		const gchar *id);
 
 G_DEFINE_TYPE (EmpathyChatWindow, empathy_chat_window, G_TYPE_OBJECT);
 
@@ -2395,7 +2393,7 @@ empathy_chat_window_has_focus (EmpathyChatWindow *window)
 	return has_focus;
 }
 
-static EmpathyChat *
+EmpathyChat *
 empathy_chat_window_find_chat (TpAccount   *account,
 			       const gchar *id)
 {
