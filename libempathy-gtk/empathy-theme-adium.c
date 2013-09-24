vitorcsantos/empathy

commit 19e784a4b2655db5cf414c7ce43662a9adb9d0c6

theme-adium: deal with g_resources_lookup_data() failing
Only empathy-chat registers the gresources so when executing this code from
the empathy process (theme preview in the preferences dialog) we don't find
the resource. That's fine as this JS code is only used for the infinite
callback but best to not warning.

https://bugzilla.gnome.org/show_bug.cgi?id=707291

libempathy-gtk/empathy-theme-adium.c
@@ -732,6 +732,9 @@ theme_adium_add_html (EmpathyThemeAdium *self,
   bytes = g_resources_lookup_data ("/org/gnome/Empathy/Chat/empathy-chat.js",
       G_RESOURCE_LOOKUP_FLAGS_NONE,
       NULL);
+  if (bytes == NULL)
+    return;
+
   js = (const gchar *) g_bytes_get_data (bytes, NULL);
   g_string_prepend (string, js);
   g_bytes_unref (bytes);

commit b205d66b7ecf9c0eb6aa89ff4f67b73bc211bc07

theme-adium: still execute the start of the script if ressource is missing
This fix a regression introduced by 19e784a4b2655db5cf414c7ce43662a9adb9d0c6.

If empathy-chat.js can't be find (like in the preferences dialog) we still
want to execute the start of the script.

https://bugzilla.gnome.org/show_bug.cgi?id=708616

libempathy-gtk/empathy-theme-adium.c
@@ -732,12 +732,13 @@ theme_adium_add_html (EmpathyThemeAdium *self,
   bytes = g_resources_lookup_data ("/org/gnome/Empathy/Chat/empathy-chat.js",
       G_RESOURCE_LOOKUP_FLAGS_NONE,
       NULL);
-  if (bytes == NULL)
-    return;
 
-  js = (const gchar *) g_bytes_get_data (bytes, NULL);
-  g_string_prepend (string, js);
-  g_bytes_unref (bytes);
+  if (bytes != NULL)
+    {
+      js = (const gchar *) g_bytes_get_data (bytes, NULL);
+      g_string_prepend (string, js);
+      g_bytes_unref (bytes);
+    }
 
   script = g_string_free (string, FALSE);
   webkit_web_view_execute_script (WEBKIT_WEB_VIEW (self), script);