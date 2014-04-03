1b4ec57cc8544f6db3d7776f10716ad342969da6
Simon McVittie <simon.mcvittie@collabora.co.uk>
2014-04-03 20:06:26 +0100

    Avoid accidentally linking to telepathy-logger's private API
    Bug: https://bugzilla.gnome.org/show_bug.cgi?id=727162

--- libempathy/action-chain-internal.h
+++ libempathy/action-chain-internal.h
@@ -24,6 +24,19 @@
 #include <glib-object.h>
 #include <gio/gio.h>
 
+/* Avoid accidentally using the symbols exported by the telepathy-logger
+ * library (GNOME#727162) */
+#define _tpl_action_chain_new_async   _empathy_action_chain_new_async
+#define _tpl_action_chain_free        _empathy_action_chain_free
+#define _tpl_action_chain_append      _empathy_action_chain_append
+#define _tpl_action_chain_prepend     _empathy_action_chain_prepend
+#define _tpl_action_chain_start       _empathy_action_chain_start
+#define _tpl_action_chain_continue    _empathy_action_chain_continue
+#define _tpl_action_chain_terminate   _empathy_action_chain_terminate
+#define _tpl_action_chain_clear       _empathy_action_chain_clear
+#define _tpl_action_chain_get_object  _empathy_action_chain_get_object
+#define _tpl_action_chain_new_finish  _empathy_action_chain_new_finish
+
 typedef struct {
     GQueue *chain;
     GSimpleAsyncResult *simple;
